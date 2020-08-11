a4333226df64d7848c9d4d68bbdef57ce7cf718a
Nhan Le <nhanlq@outlook.com>
2020-08-11 10:22:46 +0700

    update fix feedback 110820

--- web/modules/custom/external_link/src/Controller/ExternalLinkListController.php
+++ web/modules/custom/external_link/src/Controller/ExternalLinkListController.php
@@ -7,19 +7,32 @@ use Drupal\Core\Controller\ControllerBase;
 /**
  * Class ExternalLinkListController.
  */
-class ExternalLinkListController extends ControllerBase {
+class ExternalLinkListController extends ControllerBase
+{
 
-  /**
-   * List.
-   *
-   * @return string
-   *   Return Hello string.
-   */
-    public function list() {
+    /**
+     * List.
+     *
+     * @return string
+     *   Return Hello string.
+     */
+    public function list()
+    {
+        $tags = null;
+        if (isset($_GET['tags'])) {
+            $tags = $_GET['tags'];
+        }
+        $search = '';
+        if (isset($_GET['keys'])) {
+            $search = $_GET['keys'];
+        }
         return [
             'results' => [
                 '#theme' => 'external_link_list',
                 '#external_link' => $this->getExternalLink(),
+                '#tags' => $this->getTags(),
+                '#get' => $tags,
+                '#search' => $search,
             ],
             'pager' => [
                 '#type' => 'pager',
@@ -29,18 +42,70 @@ class ExternalLinkListController extends ControllerBase {
 
     public function getExternalLink()
     {
+        $tid = False;
+        if (isset($_GET['tags'])) {
+            $tags = $_GET['tags'];
+            $tid = $this->getTagsTid($tags);
+        }
 
-        $ids = \Drupal::entityQuery('external_link')
-            ->condition('status', 1)
-            ->sort('field_weight','ASC')
-            ->sort('created','DESC')
-            ->pager(10)
-            ->execute();
+
+        if ($tid) {
+            $ids = \Drupal::entityQuery('external_link')
+                ->condition('status', 1)
+                ->condition('field_tags', $tid)
+                ->sort('field_weight', 'ASC')
+                ->sort('created', 'DESC')
+                ->pager(10)
+                ->execute();
+        } elseif (isset($_GET['keys'])) {
+            $ids = \Drupal::entityQuery('external_link')
+                ->condition('status', 1)
+                ->condition('name', $_GET['keys'], 'CONTAINS')
+                ->sort('field_weight', 'ASC')
+                ->sort('created', 'DESC')
+                ->pager(10)
+                ->execute();
+        } else {
+            $ids = \Drupal::entityQuery('external_link')
+                ->condition('status', 1)
+                ->sort('field_weight', 'ASC')
+                ->sort('created', 'DESC')
+                ->pager(10)
+                ->execute();
+        }
         $result = \Drupal\external_link\Entity\ExternalLink::loadMultiple($ids);
         return $result;
     }
-    public function title(){
-        return ['#markup' => \Drupal::state()->get('/e-pharm/external-links','External Links'), '#allowed_tags' => \Drupal\Component\Utility\Xss::getHtmlTagList()];
+
+    public function getTags()
+    {
+        $tags = [];
+        $ids = \Drupal::entityQuery('external_link')
+            ->condition('status', 1)
+            ->execute();
+        $result = \Drupal\news\Entity\News::loadMultiple($ids);
+        foreach ($result as $drug) {
+            foreach ($drug->get('field_tags')->getValue() as $tag) {
+                $term = \Drupal\taxonomy\Entity\Term::load($tag['target_id']);
+                $tags[$tag['target_id']] = $term->getName();
+            }
+        }
+        return $tags;
+    }
+
+    public function getTagsTid($name)
+    {
+        $term = \Drupal::entityTypeManager()
+            ->getStorage('taxonomy_term')
+            ->loadByProperties(['name' => $name, 'vid' => 'tags']);
+        $term = reset($term);
+        $term_id = $term->id();
+        return $term_id;
+    }
+
+    public function title()
+    {
+        return ['#markup' => \Drupal::state()->get('/e-pharm/external-links', 'External Links'), '#allowed_tags' => \Drupal\Component\Utility\Xss::getHtmlTagList()];
     }
 
 }
--- web/modules/custom/public_links/src/Controller/LinksListController.php
+++ web/modules/custom/public_links/src/Controller/LinksListController.php
@@ -16,10 +16,21 @@ class LinksListController extends ControllerBase {
    *   Return Hello string.
    */
     public function list() {
+        $tags = null;
+        if (isset($_GET['tags'])) {
+            $tags = $_GET['tags'];
+        }
+        $search = '';
+        if (isset($_GET['keys'])) {
+            $search = $_GET['keys'];
+        }
         return [
             'results' => [
                 '#theme' => 'public_links_list',
                 '#public_links' => $this->getExternalLink(),
+                '#tags' => $this->getTags(),
+                '#get' => $tags,
+                '#search' => $search,
             ],
             'pager' => [
                 '#type' => 'pager',
@@ -29,15 +40,66 @@ class LinksListController extends ControllerBase {
 
     public function getExternalLink()
     {
+        $tid = False;
+        if (isset($_GET['tags'])) {
+            $tags = $_GET['tags'];
+            $tid = $this->getTagsTid($tags);
+        }
 
-        $ids = \Drupal::entityQuery('public_links')
-            ->condition('status', 1)
-            ->sort('changed','DESC')
-            ->pager(10)
-            ->execute();
+
+        if ($tid) {
+            $ids = \Drupal::entityQuery('public_links')
+                ->condition('status', 1)
+                ->condition('field_tags', $tid)
+                ->sort('field_weight', 'ASC')
+                ->sort('created', 'DESC')
+                ->pager(10)
+                ->execute();
+        } elseif (isset($_GET['keys'])) {
+            $ids = \Drupal::entityQuery('public_links')
+                ->condition('status', 1)
+                ->condition('name', $_GET['keys'], 'CONTAINS')
+                ->sort('field_weight', 'ASC')
+                ->sort('created', 'DESC')
+                ->pager(10)
+                ->execute();
+        } else {
+            $ids = \Drupal::entityQuery('public_links')
+                ->condition('status', 1)
+                ->sort('field_weight', 'ASC')
+                ->sort('created', 'DESC')
+                ->pager(10)
+                ->execute();
+        }
         $result = \Drupal\public_links\Entity\PublicLinks::loadMultiple($ids);
         return $result;
     }
+    public function getTags()
+    {
+        $tags = [];
+        $ids = \Drupal::entityQuery('public_links')
+            ->condition('status', 1)
+            ->execute();
+        $result = \Drupal\news\Entity\News::loadMultiple($ids);
+        foreach ($result as $drug) {
+            foreach ($drug->get('field_tags')->getValue() as $tag) {
+                $term = \Drupal\taxonomy\Entity\Term::load($tag['target_id']);
+                $tags[$tag['target_id']] = $term->getName();
+            }
+        }
+        return $tags;
+    }
+
+    public function getTagsTid($name)
+    {
+        $term = \Drupal::entityTypeManager()
+            ->getStorage('taxonomy_term')
+            ->loadByProperties(['name' => $name, 'vid' => 'tags']);
+        $term = reset($term);
+        $term_id = $term->id();
+        return $term_id;
+    }
+
     public function title(){
         return ['#markup' => \Drupal::state()->get('/links','Links'), '#allowed_tags' => \Drupal\Component\Utility\Xss::getHtmlTagList()];
     }
