2d4cd946442615eba7ea1e3ef84b88797e850bff
Nhan Le <nhanlq@outlook.com>
2020-10-05 14:27:25 +0700

    add new ads

--- web/modules/custom/advert/src/Plugin/Block/Adsright.php
+++ web/modules/custom/advert/src/Plugin/Block/Adsright.php
@@ -14,15 +14,82 @@ use Drupal\Core\Block\BlockBase;
  */
 class Adsright extends BlockBase {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function build() {
-    $build = [];
-    $build['#theme'] = 'adsright';
-     $build['adsright']['#markup'] = 'Implement Adsright.';
+    /**
+     * {@inheritdoc}
+     */
+    public function build()
+    {
+        return [
+            '#theme' => 'ads_list',
+            '#ads' => $this->checkLeft(),
+            '#cache' => [
+                'max-age' => 0,
+            ],
+        ];
+    }
 
-    return $build;
-  }
+    public function getAdsLeft()
+    {
+        $ids = \Drupal::entityQuery('ads')
+            ->condition('status', 1)
+            ->condition('field_ads_type', 'right')
+            ->sort('field_weight', 'ASC')
+            ->execute();
+        $result = \Drupal\advert\Entity\Ads::loadMultiple($ids);
+        return $result;
+    }
+
+    public function checkLeft()
+    {
+        $ads = $this->getAdsLeft();
+
+        $current_path = \Drupal::service('path.current')->getPath();
+        $pathArr = explode('/', $current_path);
+        $data = [];
+        foreach ($ads as $ad) {
+            if ($ad->get('field_path')->getValue()) {
+                foreach ($ad->get('field_path')->getValue() as $path) {
+                    if ($path['value'] == $current_path) {
+                        $data[] = $ad;
+                    }
+                    if($path['value']=='<front>'){
+                        if(\Drupal::service('path.matcher')->isFrontPage()){
+                            $data[] = $ad;
+                        }
+                    }
+                }
+            }
+            if ($ad->get('field_section')->getValue()) {
+
+                foreach ($ad->get('field_section')->getValue() as $section) {
+
+                    if ($pathArr[1] == 'e-pharm') {
+                        if ($pathArr[2] == $section['value']) {
+                            $data[] = $ad;
+                        }
+                    }elseif ($pathArr[1] == 'cme') {
+                        if ($pathArr[2] == $section['value']) {
+                            $data[] = $ad;
+                        }
+                        if ($pathArr[2] = 'links') {
+                            if ($section['value'] == 'cme-links') {
+                                $data[] = $ad;
+                            }
+                        }
+                    }else{
+
+                        if($pathArr[1] == $section['value']){
+                            $data[] = $ad;
+                        }
+                    }
+
+
+                }
+            }
+        }
+        // var_dump($data);
+        return $data;
+
+    }
 
 }
