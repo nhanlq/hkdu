339459df708d1ea6793b691f8e6066e16f040092
ffw-nhan-le <nhanlq@outlook.com>
2021-07-09 10:31:32 +0700

    uypdate

--- web/modules/custom/cme_quiz/src/Controller/QuizListController.php
+++ web/modules/custom/cme_quiz/src/Controller/QuizListController.php
@@ -3,6 +3,8 @@
 namespace Drupal\cme_quiz\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
 
 /**
  * Class QuizListController.
@@ -16,193 +18,333 @@ class QuizListController extends ControllerBase {
    *   Return Hello string.
    */
   public function quizlist() {
-      $tags = null;
-      if (isset($_GET['tags'])) {
-          $tags = $_GET['tags'];
-      }
-      $search = '';
-      if (isset($_GET['keys'])) {
-          $search = $_GET['keys'];
-      }
-      return [
-          'results' => [
-              '#theme' => 'quiz_list',
-              '#quizs' => $this->getQuiz(),
-              '#tags' => $this->getTags(),
-              '#get' => $tags,
-              '#search' => $search,
-              '#cache' => [
-                  'max-age' => 0,
-              ],
-          ],
-          'pager' => [
-              '#type' => 'pager',
-          ],
-      ];
+    $tags = NULL;
+    if (isset($_GET['tags'])) {
+      $tags = $_GET['tags'];
+    }
+    $search = '';
+    if (isset($_GET['keys'])) {
+      $search = $_GET['keys'];
+    }
+    return [
+      'results' => [
+        '#theme' => 'quiz_list',
+        '#quizs' => $this->getQuiz(),
+        '#tags' => $this->getTags(),
+        '#get' => $tags,
+        '#search' => $search,
+        '#cache' => [
+          'max-age' => 0,
+        ],
+      ],
+      'pager' => [
+        '#type' => 'pager',
+      ],
+    ];
   }
-    public function getQuiz()
-    {
-        $tid = False;
-        if (isset($_GET['tags'])) {
-            $tags = $_GET['tags'];
-            $tid = $this->getTagsTid($tags);
-        }
-        $currentDate = time();
 
-        if ($tid) {
-            $ids = \Drupal::entityQuery('quiz')
-                ->condition('status', 1)
-                ->condition('field_category', $tid)
-                ->condition('field_expired',$currentDate,'>=')
-                ->sort('field_weight','ASC')
-                ->sort('created','DESC')
-                ->pager(15)
-                ->execute();
-        } elseif (isset($_GET['keys'])) {
-            $ids = \Drupal::entityQuery('quiz')
-                ->condition('status', 1)
-                ->condition('name', $_GET['keys'], 'CONTAINS')
-                ->condition('field_expired',$currentDate,'>=')
-                ->sort('field_weight','ASC')
-                ->sort('created','DESC')
-                ->pager(15)
-                ->execute();
-        } else {
-            $ids = \Drupal::entityQuery('quiz')
-                ->condition('status', 1)
-                ->condition('field_expired',$currentDate,'>=')
-                ->sort('field_weight','ASC')
-                ->sort('created','DESC')
-                ->pager(15)
-                ->execute();
-        }
-        $result = \Drupal\cme_quiz\Entity\Quiz::loadMultiple($ids);
-        foreach($result as $r){
-
-            $r->author = $this->getAuthor($r->getOwnerId());
-            if($this->checkExpiredQuiz($r)){
-                $r->expired = true;
-            }else{
-                $r->expired = false;
-            }
-            if($this->checkQuizStatusUser($r->id())){
-                $r->check_event = true;
-                if($this->checkUserAttempStatus($r)){
-                    $r->attemp = 'Passed';
-                }else{
-                    $r->attemp = 'Attempted';
-                }
-            }else{
-                $r->check_event = false;
-            }
-        }
-        return $result;
+  /**
+   * Quizlist.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function quizlistarchived() {
+    $tags = NULL;
+    if (isset($_GET['tags'])) {
+      $tags = $_GET['tags'];
     }
+    $search = '';
+    if (isset($_GET['keys'])) {
+      $search = $_GET['keys'];
+    }
+    return [
+      'results' => [
+        '#theme' => 'quiz_list',
+        '#quizs' => $this->getQuizArchived(),
+        '#tags' => $this->getTags(),
+        '#get' => $tags,
+        '#search' => $search,
+        '#cache' => [
+          'max-age' => 0,
+        ],
+      ],
+      'pager' => [
+        '#type' => 'pager',
+      ],
+    ];
+  }
 
-    public function getTags()
-    {
-        $tags = [];
-        $ids = \Drupal::entityQuery('quiz')
-            ->condition('status', 1)
-            ->execute();
-        $result = \Drupal\cme_quiz\Entity\Quiz::loadMultiple($ids);
-        foreach ($result as $drug) {
-            foreach ($drug->get('field_category')->getValue() as $tag) {
-                $term = \Drupal\taxonomy\Entity\Term::load($tag['target_id']);
-                $tags[$tag['target_id']] = $term->getName();
-            }
+  /**
+   * @return \Drupal\cme_quiz\Entity\Quiz[]|\Drupal\Core\Entity\EntityBase[]|\Drupal\Core\Entity\EntityInterface[]
+   */
+  public function getQuiz() {
+    $tid = FALSE;
+    if (isset($_GET['tags'])) {
+      $tags = $_GET['tags'];
+      $tid = $this->getTagsTid($tags);
+    }
+    $now = new DrupalDateTime('now');
+    if ($tid) {
+      $ids = \Drupal::entityQuery('quiz')
+        ->condition('status', 1)
+        ->condition('field_category', $tid)
+        ->condition('field_expired', $now->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT), '>=')
+        ->sort('field_weight', 'ASC')
+        ->sort('created', 'DESC')
+        ->pager(15)
+        ->execute();
+    }
+    elseif (isset($_GET['keys'])) {
+      $ids = \Drupal::entityQuery('quiz')
+        ->condition('status', 1)
+        ->condition('name', $_GET['keys'], 'CONTAINS')
+        ->condition('field_expired', $now->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT), '>=')
+        ->sort('field_weight', 'ASC')
+        ->sort('created', 'DESC')
+        ->pager(15)
+        ->execute();
+    }
+    else {
+      $ids = \Drupal::entityQuery('quiz')
+        ->condition('status', 1)
+        ->condition('field_expired', $now->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT), '>=')
+        ->sort('field_weight', 'ASC')
+        ->sort('created', 'DESC')
+        ->pager(15)
+        ->execute();
+    }
+    $result = \Drupal\cme_quiz\Entity\Quiz::loadMultiple($ids);
+    foreach ($result as $r) {
+
+      $r->author = $this->getAuthor($r->getOwnerId());
+      if ($this->checkExpiredQuiz($r)) {
+        $r->expired = TRUE;
+      }
+      else {
+        $r->expired = FALSE;
+      }
+      if ($this->checkQuizStatusUser($r->id())) {
+        $r->check_event = TRUE;
+        if ($this->checkUserAttempStatus($r)) {
+          $r->attemp = 'Passed';
         }
-        return $tags;
-    }
-
-    public function getTagsTid($name)
-    {
-        $term = \Drupal::entityTypeManager()
-            ->getStorage('taxonomy_term')
-            ->loadByProperties(['name' => $name, 'vid' => 'quiz']);
-        $term = reset($term);
-        $term_id = $term->id();
-        return $term_id;
-    }
-
-
-    /**
-     * get product by Event
-     */
-    public function getQuestions($quiz_id){
-        $ids = \Drupal::entityQuery('question')
-            ->condition('type', 'default')
-            ->condition('status', 1)
-            ->condition('field_quiz', $quiz_id)
-            ->execute();
-        $results = \Drupal\cme_question\Entity\Question::loadMultiple($ids);
-        $result = reset($results);
-        return $result;
-    }
-
-    public function checkQuizStatusUser($quiz_id){
-        $return = null;
-        $user = \Drupal::currentUser();
-        $uid = $user->id();
-        $ids = \Drupal::entityQuery('result')
-            ->condition('status', 1)
-            ->condition('field_quiz', $quiz_id)
-            ->condition('field_user', $uid)
-            ->execute();
-        $results = \Drupal\cme_result\Entity\Result ::loadMultiple($ids);
-        if($results){
-            $return = true;
+        else {
+          $r->attemp = 'Attempted';
         }
-        return $return;
+      }
+      else {
+        $r->check_event = FALSE;
+      }
     }
+    return $result;
+  }
 
-    public function checkExpiredQuiz($quiz){
-        $current = time();
-        $date = strtotime($quiz->get('field_expired')->value);
-        if($current > $date){
-            return true;
-        }else{
-            return false;
-        }
+  /**
+   * @return \Drupal\cme_quiz\Entity\Quiz[]|\Drupal\Core\Entity\EntityBase[]|\Drupal\Core\Entity\EntityInterface[]
+   */
+  public function getQuizArchived() {
+    $tid = FALSE;
+    if (isset($_GET['tags'])) {
+      $tags = $_GET['tags'];
+      $tid = $this->getTagsTid($tags);
     }
-    public function checkUserAttempStatus($quiz){
-        $return = null;
-        $user = \Drupal::currentUser();
-        $uid = $user->id();
-        $ids = \Drupal::entityQuery('result')
-            ->condition('status', 1)
-            ->condition('field_quiz', $quiz->id())
-            ->condition('field_user', $uid)
-            ->execute();
-        $results = \Drupal\cme_result\Entity\Result ::loadMultiple($ids);
-        if($results){
-           foreach($results as $result){
-               if($result->get('field_passed')->value == 1){
-                   $return = true;
-                   break;
-               }
-           }
-        }
-        return $return;
+    $now = new DrupalDateTime('now');
+    if ($tid) {
+      $ids = \Drupal::entityQuery('quiz')
+        ->condition('status', 1)
+        ->condition('field_category', $tid)
+        ->condition('field_expired', $now->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT), '<')
+        ->sort('field_weight', 'ASC')
+        ->sort('created', 'DESC')
+        ->pager(15)
+        ->execute();
     }
-    public function getAuthor($uid){
+    elseif (isset($_GET['keys'])) {
+      $ids = \Drupal::entityQuery('quiz')
+        ->condition('status', 1)
+        ->condition('name', $_GET['keys'], 'CONTAINS')
+        ->condition('field_expired', $now->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT), '<')
+        ->sort('field_weight', 'ASC')
+        ->sort('created', 'DESC')
+        ->pager(15)
+        ->execute();
+    }
+    else {
+      $ids = \Drupal::entityQuery('quiz')
+        ->condition('status', 1)
+        ->condition('field_expired', $now->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT), '<')
+        ->sort('field_weight', 'ASC')
+        ->sort('created', 'DESC')
+        ->pager(15)
+        ->execute();
+    }
+    $result = \Drupal\cme_quiz\Entity\Quiz::loadMultiple($ids);
+    foreach ($result as $r) {
 
-        $user = \Drupal\user\Entity\User::load($uid);
-        $account = $user->toArray();
-        $name = '';
-        if($account['field_first_name']){
-            $name .= $account['field_first_name'][0]['value'];
+      $r->author = $this->getAuthor($r->getOwnerId());
+      if ($this->checkExpiredQuiz($r)) {
+        $r->expired = TRUE;
+      }
+      else {
+        $r->expired = FALSE;
+      }
+      if ($this->checkQuizStatusUser($r->id())) {
+        $r->check_event = TRUE;
+        if ($this->checkUserAttempStatus($r)) {
+          $r->attemp = 'Passed';
         }
-        if($account['field_last_name']){
-            $name .= ' '.$account['field_last_name'][0]['value'];
+        else {
+          $r->attemp = 'Attempted';
         }
-        if($name == ''){
-            $name .= $account['name'][0]['value'];
-        }
-        return $name;
+      }
+      else {
+        $r->check_event = FALSE;
+      }
     }
+    return $result;
+  }
 
-    public function title(){
-        return ['#markup' => \Drupal::state()->get('/cme/quiz','CME Quizzes'), '#allowed_tags' => \Drupal\Component\Utility\Xss::getHtmlTagList()];
+  /**
+   * @return array
+   */
+  public function getTags() {
+    $tags = [];
+    $ids = \Drupal::entityQuery('quiz')->condition('status', 1)->execute();
+    $result = \Drupal\cme_quiz\Entity\Quiz::loadMultiple($ids);
+    foreach ($result as $drug) {
+      foreach ($drug->get('field_category')->getValue() as $tag) {
+        $term = \Drupal\taxonomy\Entity\Term::load($tag['target_id']);
+        $tags[$tag['target_id']] = $term->getName();
+      }
     }
+    return $tags;
+  }
+
+  /**
+   * @param $name
+   *
+   * @return int|string|null
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function getTagsTid($name) {
+    $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadByProperties([
+        'name' => $name,
+        'vid' => 'quiz',
+      ]);
+    $term = reset($term);
+    $term_id = $term->id();
+    return $term_id;
+  }
+
+
+  /**
+   * get product by Event
+   */
+  public function getQuestions($quiz_id) {
+    $ids = \Drupal::entityQuery('question')
+      ->condition('type', 'default')
+      ->condition('status', 1)
+      ->condition('field_quiz', $quiz_id)
+      ->execute();
+    $results = \Drupal\cme_question\Entity\Question::loadMultiple($ids);
+    $result = reset($results);
+    return $result;
+  }
+
+  /**
+   * @param $quiz_id
+   *
+   * @return bool|null
+   */
+  public function checkQuizStatusUser($quiz_id) {
+    $return = NULL;
+    $user = \Drupal::currentUser();
+    $uid = $user->id();
+    $ids = \Drupal::entityQuery('result')
+      ->condition('status', 1)
+      ->condition('field_quiz', $quiz_id)
+      ->condition('field_user', $uid)
+      ->execute();
+    $results = \Drupal\cme_result\Entity\Result::loadMultiple($ids);
+    if ($results) {
+      $return = TRUE;
+    }
+    return $return;
+  }
+
+  /**
+   * @param $quiz
+   *
+   * @return bool
+   */
+  public function checkExpiredQuiz($quiz) {
+    $current = time();
+    $date = strtotime($quiz->get('field_expired')->value);
+    if ($current > $date) {
+      return TRUE;
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+  /**
+   * @param $quiz
+   *
+   * @return bool|null
+   */
+  public function checkUserAttempStatus($quiz) {
+    $return = NULL;
+    $user = \Drupal::currentUser();
+    $uid = $user->id();
+    $ids = \Drupal::entityQuery('result')
+      ->condition('status', 1)
+      ->condition('field_quiz', $quiz->id())
+      ->condition('field_user', $uid)
+      ->execute();
+    $results = \Drupal\cme_result\Entity\Result::loadMultiple($ids);
+    if ($results) {
+      foreach ($results as $result) {
+        if ($result->get('field_passed')->value == 1) {
+          $return = TRUE;
+          break;
+        }
+      }
+    }
+    return $return;
+  }
+
+  /**
+   * @param $uid
+   *
+   * @return string
+   */
+  public function getAuthor($uid) {
+
+    $user = \Drupal\user\Entity\User::load($uid);
+    $account = $user->toArray();
+    $name = '';
+    if ($account['field_first_name']) {
+      $name .= $account['field_first_name'][0]['value'];
+    }
+    if ($account['field_last_name']) {
+      $name .= ' ' . $account['field_last_name'][0]['value'];
+    }
+    if ($name == '') {
+      $name .= $account['name'][0]['value'];
+    }
+    return $name;
+  }
+
+  /**
+   * @return array
+   */
+  public function title() {
+    return [
+      '#markup' => \Drupal::state()->get('/cme/quiz', 'CME Quizzes'),
+      '#allowed_tags' => \Drupal\Component\Utility\Xss::getHtmlTagList(),
+    ];
+  }
 }
