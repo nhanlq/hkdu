8da167ec420a43c966095171235d862d4a91e55a
ffw-nhan-le <nhanlq@outlook.com>
2021-08-06 09:37:33 +0700

    update tasks

--- web/modules/custom/event/src/Controller/EventListController.php
+++ web/modules/custom/event/src/Controller/EventListController.php
@@ -17,149 +17,211 @@ class EventListController extends ControllerBase {
    * @return string
    *   Return Hello string.
    */
-    public function list()
-    {
-        $tags = null;
-        if (isset($_GET['tags'])) {
-            $tags = $_GET['tags'];
-        }
-        $search = '';
-        if (isset($_GET['keys'])) {
-            $search = $_GET['keys'];
-        }
-        return [
-            'results' => [
-                '#theme' => 'event_list',
-                '#events' => $this->getEvent(),
-                '#tags' => $this->getTags(),
-                '#get' => $tags,
-                '#search' => $search,
-            ],
-            'pager' => [
-                '#type' => 'pager',
-            ],
-        ];
+  public function list() {
+    $tags = NULL;
+    if (isset($_GET['tags'])) {
+      $tags = $_GET['tags'];
     }
-
-    public function getEvent()
-    {
-        $tid = False;
-        if (isset($_GET['tags'])) {
-            $tags = $_GET['tags'];
-            $tid = $this->getTagsTid($tags);
-        }
-        $currentDate = time();
-
-        if ($tid) {
-            $ids = \Drupal::entityQuery('event')
-                ->condition('status', 1)
-                ->condition('field_tags', $tid)
-                ->condition('field_expired',$currentDate,'>=')
-                ->sort('field_weight','ASC')
-                ->sort('field_date','ASC')
-                ->pager(15)
-                ->execute();
-        } elseif (isset($_GET['keys'])) {
-            $ids = \Drupal::entityQuery('event')
-                ->condition('status', 1)
-                ->condition('name', $_GET['keys'], 'CONTAINS')
-                ->condition('field_expired',$currentDate,'>=')
-                ->sort('field_weight','ASC')
-                ->sort('field_date','ASC')
-                ->pager(15)
-                ->execute();
-        } else {
-            $ids = \Drupal::entityQuery('event')
-                ->condition('status', 1)
-                ->condition('field_expired',$currentDate,'>=')
-                ->sort('field_weight','ASC')
-                ->sort('field_date','ASC')
-                ->pager(15)
-                ->execute();
-        }
-        $result = \Drupal\event\Entity\Event::loadMultiple($ids);
-        foreach($result as $r){
-            if($this->checkExpiredEvent($r)){
-                $r->expired = true;
-            }else{
-                $r->expired = false;
-            }
-            if($this->checkEventStatusUser($r->id())){
-                $r->check_event = true;
-            }else{
-                $r->check_event = false;
-            }
-        }
-        return $result;
+    $search = '';
+    if (isset($_GET['keys'])) {
+      $search = $_GET['keys'];
     }
+    return [
+      'results' => [
+        '#theme' => 'event_list',
+        '#events' => $this->getEvent(),
+        '#tags' => $this->getTags(),
+        '#get' => $tags,
+        '#search' => $search,
+      ],
+      'pager' => [
+        '#type' => 'pager',
+      ],
+    ];
+  }
 
-    public function getTags()
-    {
-        $tags = [];
-        $ids = \Drupal::entityQuery('event')
-            ->condition('status', 1)
-            ->execute();
-        $result = \Drupal\event\Entity\Event::loadMultiple($ids);
-        foreach ($result as $drug) {
-            foreach ($drug->get('field_tags')->getValue() as $tag) {
-                $term = Term::load($tag['target_id']);
-                $tags[$tag['target_id']] = $term->getName();
-            }
-        }
-        return $tags;
+  /**
+   * @return \Drupal\Core\Entity\EntityBase[]|\Drupal\Core\Entity\EntityInterface[]|\Drupal\event\Entity\Event[]
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function getEvent() {
+    $tid = FALSE;
+    if (isset($_GET['tags'])) {
+      $tags = $_GET['tags'];
+      $tid = $this->getTagsTid($tags);
     }
+    $currentDate = time();
 
-    public function getTagsTid($name)
-    {
-        $term = \Drupal::entityTypeManager()
-            ->getStorage('taxonomy_term')
-            ->loadByProperties(['name' => $name, 'vid' => 'event']);
-        $term = reset($term);
-        $term_id = $term->id();
-        return $term_id;
+    if ($tid) {
+      $ids = \Drupal::entityQuery('event')
+        ->condition('status', 1)
+        ->condition('field_tags', $tid)
+        ->condition('field_expired', $currentDate, '>=')
+        ->sort('field_weight', 'ASC')
+        ->sort('field_date', 'ASC')
+        ->pager(15)
+        ->execute();
     }
+    elseif (isset($_GET['keys'])) {
+      $ids = \Drupal::entityQuery('event')
+        ->condition('status', 1)
+        ->condition('name', $_GET['keys'], 'CONTAINS')
+        ->condition('field_expired', $currentDate, '>=')
+        ->sort('field_weight', 'ASC')
+        ->sort('field_date', 'ASC')
+        ->pager(15)
+        ->execute();
+    }
+    else {
+      $ids = \Drupal::entityQuery('event')
+        ->condition('status', 1)
+        ->condition('field_expired', $currentDate, '>=')
+        ->sort('field_weight', 'ASC')
+        ->sort('field_date', 'ASC')
+        ->pager(15)
+        ->execute();
+    }
+    $result = \Drupal\event\Entity\Event::loadMultiple($ids);
+    foreach ($result as $r) {
+      if ($this->checkExpiredEvent($r)) {
+        $r->expired = TRUE;
+      }
+      else {
+        $r->expired = FALSE;
+      }
+      if ($this->checkEventStatusUser($r->id())) {
+        $r->check_event = TRUE;
+      }
+      else {
+        $r->check_event = FALSE;
+      }
+    }
+    return $result;
+  }
+
+  /**
+   * @return array
+   */
+  public function getTags() {
+    $tags = [];
+    $ids = \Drupal::entityQuery('event')->condition('status', 1)->execute();
+    $result = \Drupal\event\Entity\Event::loadMultiple($ids);
+    foreach ($result as $drug) {
+      foreach ($drug->get('field_tags')->getValue() as $tag) {
+        $term = Term::load($tag['target_id']);
+        $tags[$tag['target_id']] = $term->getName();
+      }
+    }
+    return $tags;
+  }
+
+  /**
+   * @param $name
+   *
+   * @return int|string|null
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function getTagsTid($name) {
+    $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadByProperties([
+        'name' => $name,
+        'vid' => 'event',
+      ]);
+    $term = reset($term);
+    $term_id = $term->id();
+    return $term_id;
+  }
 
 
-    /**
-     * get product by Event
-     */
-    public function getProduct($event_id){
-        $ids = \Drupal::entityQuery('commerce_product')
-            ->condition('type', 'default')
-            ->condition('status', 1)
-            ->condition('field_event', $event_id)
-            ->execute();
-        $results = \Drupal\commerce_product\Entity\Product::loadMultiple($ids);
-        $result = reset($results);
-        return $result;
-    }
+  /**
+   * get product by Event
+   */
+  public function getProduct($event_id) {
+    $ids = \Drupal::entityQuery('commerce_product')
+      ->condition('type', 'default')
+      ->condition('status', 1)
+      ->condition('field_event', $event_id)
+      ->execute();
+    $results = \Drupal\commerce_product\Entity\Product::loadMultiple($ids);
+    $result = reset($results);
+    return $result;
+  }
 
-    public function checkEventStatusUser($event_id){
-        $return = null;
-        $user = \Drupal::currentUser();
-        $uid = $user->id();
-        $ids = \Drupal::entityQuery('event_tracking')
-            ->condition('status', 1)
-            ->condition('field_event', $event_id)
-            ->condition('field_user', $uid)
-            ->execute();
-        $results = \Drupal\event_tracking\Entity\EventTracking::loadMultiple($ids);
-        if($results){
-            $return = true;
-        }
-        return $return;
+  /**
+   * @param $event_id
+   *
+   * @return bool|null
+   */
+  public function checkEventStatusUser($event_id) {
+    $return = NULL;
+    $user = \Drupal::currentUser();
+    $uid = $user->id();
+    $ids = \Drupal::entityQuery('event_tracking')
+      ->condition('status', 1)
+      ->condition('field_event', $event_id)
+      ->condition('field_user', $uid)
+      ->execute();
+    $results = \Drupal\event_tracking\Entity\EventTracking::loadMultiple($ids);
+    if ($results) {
+      $return = TRUE;
     }
+    return $return;
+  }
 
-    public function checkExpiredEvent($event){
-        $current = time();
-        $date = strtotime($event->get('field_date')->value.' 00:00:00');
-        if($current > $date){
-            return true;
-        }else{
-            return false;
-        }
+  /**
+   * @param $event
+   *
+   * @return bool
+   */
+  public function checkExpiredEvent($event) {
+    $current = time();
+    $date = strtotime($event->get('field_date')->value . ' 00:00:00');
+    if ($current > $date) {
+      return TRUE;
     }
-    public function title(){
-        return ['#markup' => \Drupal::state()->get('/e-pharm/event','Events'), '#allowed_tags' => \Drupal\Component\Utility\Xss::getHtmlTagList()];
+    else {
+      return FALSE;
     }
+  }
+
+  /**
+   * @return array
+   */
+  public function title() {
+    return [
+      '#markup' => \Drupal::state()->get('/e-pharm/event', 'Events'),
+      '#allowed_tags' => \Drupal\Component\Utility\Xss::getHtmlTagList(),
+    ];
+  }
+
+  public function list_title($id) {
+    $event = \Drupal\event\Entity\Event::load($id);
+    return [
+      '#markup' => $event->get('name')->value,
+      '#allowed_tags' => \Drupal\Component\Utility\Xss::getHtmlTagList(),
+    ];
+  }
+
+  /**
+   * @param $id
+   *
+   * @return \Drupal\Core\Entity\EntityBase[]|\Drupal\Core\Entity\EntityInterface[]|\Drupal\event_tracking\Entity\EventTracking[]|false
+   */
+  public function enrollment($id){
+    $ids = \Drupal::entityQuery('event_tracking')
+      ->condition('field_event', $id)
+      ->execute();
+    $results = \Drupal\event_tracking\Entity\EventTracking::loadMultiple($ids);
+    if ($results) {
+      $scores = $results;
+    }
+    return [
+      '#theme' => 'event_enrollment_list',
+      '#scores' => $scores,
+      '#cache' => [
+        'max-age' => 0,
+      ],
+    ];
+  }
 }
